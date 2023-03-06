1e8831f8d9d2320bd4d664bf3d640b4e93b67e53
Cherish Sarmiento <111546406+cofltm@users.noreply.github.com>
2023-03-06 15:11:50 +0800

    Create NonFiction.java

---
+++ Number4/NonFiction.java
@@ -0,0 +1,13 @@
+public class NonFiction extends Book {
+        
+    public NonFiction (String booksTitle) {
+        this.booksTitle = booksTitle;
+        this.booksPrice = 375.95;
+    }
+    
+    @Override
+    public void setPrice() {
+//        System.out.println(375.95);
+//        booksPrice = 375.95;
+    }
+}
